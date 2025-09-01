d1ef4b40b12cf9e8e1d2b5cffb8937d4b6dc25e1
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-01 15:20:19 +0500

    first lesson

--- Прикладное программирование/1.py
+++ Прикладное программирование/1.py
@@ -1,2 +1,33 @@
-print(1)
+"""
+int целочисленный 1 234  45352314214
+float 1.234 1.2345678901
+str "123456789012345678901234567890"
+str "text"
+str '12-12-2012 Количество кликов: 21'
 
+bool True False
+
+list
+set
+dict
+tuple
+frozenset
+"""
+
+print(1123)
+str1 = '''
+длинная 
+строка
+'''
+
+amount_click = 12
+float1 = 1.234
+date = '12-12-2012'
+register_date = '12-12-2012'
+
+print(type(date))
+print(type(amount_click).__name__)
+print(type(register_date).__name__)
+
+flag = True
+flag2 = False
