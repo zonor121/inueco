575ce926265df686e4d5941b0922508ed2088d42
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-01 19:33:18 +0500

    pz 1

---
+++ Управление качеством и надежность информационных систем/lesson 1/boolean.py
@@ -0,0 +1,26 @@
+flag1 = True
+flag2 = False
+
+flag1 = 5 > 2 
+print(flag1)
+
+"""
+>
+<
+>=
+<=
+!=
+==
+"""
+flag2 = 5 == 2
+print(flag2)
+flag3 = 5 != 2
+print(flag3)
+"""
+and
+or
+not
+"""
+big_bool = True and 5 < 7 or "123" != "123" or not False
+print(big_bool)
+print(True and 5 < 7 or "123" != "123" or not False)
