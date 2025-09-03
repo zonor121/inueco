7bd2c65a5395998f0c7aa224d2d3b6c3efab4baa
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-03 15:12:25 +0500

    условный оператор

---
+++ Архитектура информационных систем/lesson2/func_print.py
@@ -0,0 +1,4 @@
+number = 5
+print(1, 1, 1, 1, 1)
+print(1, 1, 1, 1, 1, sep="-", end="")
+print(1, 1, 1, 1, 1, sep=",", end="")
---
+++ Архитектура информационных систем/lesson2/ifelseelif.py
@@ -0,0 +1,59 @@
+"""
+if - если, с пробелом после if, с : в конце
+если в условии образуется True, то выполняется первый блок кода внутри if
+если в условии образуется False, то выполняется второй блок кода внутри else
+else без условия, но с : в конце
+else не обязателен
+"""
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("You are eligible to vote")
+    print("You are eligible to vote")
+    print("You are eligible to vote")
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+if True and 6 < 5 or not False:
+    print("True")
+
+flag1 = True
+
+if flag1:
+    print("True")
+
+"""
+Flase:
+0
+""
+''
+[]
+()
+{}
+set()
+frozenset()
+
+True:
+когда есть что-то
+"""
+
+if 0:
+    if 0:
+        print("True")
+    else:
+        print("False")
+else:
+    print("False")
+
+
+mark = int(input("Enter your mark: "))
+
+if mark >= 80:
+    print("A")
+elif mark >= 60:
+    print("B")
+elif mark >= 40:
+    print("C")
+else:
+    print("D")
