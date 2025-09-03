8713e676e5d44e912a8024c26f16907b39bfaa99
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-03 14:28:52 +0500

    pz 1

---
+++ Архитектура информационных систем/lesson2/func_input.py
@@ -0,0 +1,61 @@
+"""
+функция с ()
+print() - для вывода на консоль
+type() - узнать тип данных
+input() - для ввода с клавиатуры
+int()
+float()
+str()
+bool()
+вложенность доступна только с функциями, возвращающими значение
+"""
+
+# number = input()
+# number = int(number)
+# print(number + 5)
+print(int(float("1.1")))
+print(print())
+"""
+int
+float
+str
+bool
+list
+set
+dict
+tuple
+frozenset
+
+int()
+float()
+str()
+bool()
+
+list()
+set()
+dict()
+tuple()
+frozenset()
+"""
+
+# bool
+
+flag1 = True
+flag2 = False
+"""
+>
+<
+>=
+<=
+!=
+==
+and логическое и *
+or логическое или +
+not отрицание
+"""
+number = 5
+result = 2 < number < 78
+print(result)
+
+result = True and 7 < 54 and int('23') < 123 or 6 * 7 != 42 and not False
+print(result)
