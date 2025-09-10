f0d2412c7e74755021995db6b05acf072c6e9635
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-10 14:09:39 +0500

    pz 3 done

--- Архитектура информационных систем/lesson2/3pz.py
+++ Архитектура информационных систем/lesson2/3pz.py
@@ -4,3 +4,33 @@
 # 4. Выведите результат и его тип
 # 5. Повторять все заново с использованием while
 # 6. Завершить программу если пользователь введет 'exit'
+def custom_input(is_operation: bool = None):
+    if is_operation:
+        operation = input("Введите операцию: ")
+        if operation != "exit":
+            return operation
+    else:
+        number = input("Введите число: ")
+        if number != "exit":
+            return int(number)
+    raise Exception("exit")
+
+while True:
+    try:
+        n1 = custom_input()
+        n2 = custom_input()
+        operation = custom_input(True)
+        if operation == "+":
+            print(n1 + n2)
+        elif operation == "-":
+            print(n1 - n2)
+        elif operation == "*":
+            print(n1 * n2)
+        elif operation == '/':
+            print(n1 / n2)
+        else:
+            raise Exception('invalid operation')
+
+    except Exception as e:
+        print(e)
+        break
