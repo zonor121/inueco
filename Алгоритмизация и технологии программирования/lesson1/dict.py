aa9f802ea2a33bfcb8224328ae6d828026caf250
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-15 14:09:19 +0500

    fisrt dict

---
+++ Алгоритмизация и технологии программирования/lesson1/dict.py
@@ -0,0 +1,17 @@
+zero_dict = {}
+
+translatoin = {
+    "ананас": "pineapple",
+    "яблоко": "apple",
+}
+
+dict1 = { "1": 123, "2": 234, }
+
+print(translatoin)
+print(zero_dict)
+print(dict1)
+
+print(translatoin["ананас"])
+print(translatoin["яблоко"])
+
+print(translatoin["яблоко2"])
