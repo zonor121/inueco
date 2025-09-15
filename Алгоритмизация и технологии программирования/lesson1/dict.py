63bfbb561bbcb322e06ab79bc0ac272c7dc9c65b
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-09-15 14:52:39 +0500

    словари

--- Алгоритмизация и технологии программирования/lesson1/dict.py
+++ Алгоритмизация и технологии программирования/lesson1/dict.py
@@ -1,17 +1,17 @@
 zero_dict = {}
 
-translatoin = {
+translation = {
     "ананас": "pineapple",
     "яблоко": "apple",
 }
 
 dict1 = { "1": 123, "2": 234, }
 
-print(translatoin)
+print(translation)
 print(zero_dict)
 print(dict1)
 
-print(translatoin["ананас"])
-print(translatoin["яблоко"])
+print(translation["ананас"])
+print(translation["яблоко"])
 
-print(translatoin["яблоко2"])
+print(translation.get("яблоко2", "яблоко2 нет в словаре"))
--- Архитектура информационных систем/lesson2/func_print.py
+++ Архитектура информационных систем/lesson2/func_print.py
@@ -1,4 +1,22 @@
+from pprint import pprint
+
 number = 5
 print(1, 1, 1, 1, 1)
 print(1, 1, 1, 1, 1, sep="-", end="")
 print(1, 1, 1, 1, 1, sep=",", end="")
+
+person = {
+    'name': 'alice',
+    'age': 24,
+    'address': {
+        'city': 'new york',
+        'zip': '10101'
+    },
+    'phones': [
+        88005553535,
+        88005553536,
+    ],
+}
+
+print(person)
+pprint(person, indent=4)
