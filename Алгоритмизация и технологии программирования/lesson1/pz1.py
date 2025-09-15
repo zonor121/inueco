a676ed60c1afd1aa0fdeb8aec53c9602162dddc8
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-09-15 15:01:03 +0500

    pz1

---
+++ Алгоритмизация и технологии программирования/lesson1/pz1.py
@@ -0,0 +1,24 @@
+students = {
+    101: {
+        "name": "Иван Иванов",
+        "age": 20,
+        "grades": {"math": 4, "physics": 5, "programming": 5},
+        "group": "ИТ-101"
+    },
+    102: {
+        "name": "Мария Петрова", 
+        "age": 19,
+        "grades": {"math": 5, "physics": 4, "programming": 4},
+        "group": "ИТ-101"
+    },
+    103: {
+        "name": "Алексей Сидоров",
+        "age": 21, 
+        "grades": {"math": 3, "physics": 3, "programming": 4},
+        "group": "ИТ-102"
+    }
+}
+
+"""
+найти лучше студента по среднему баллу
+"""
