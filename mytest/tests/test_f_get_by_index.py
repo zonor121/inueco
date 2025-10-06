be2854b47bd0b2de18d31da5d83af759d9197b91
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2025-10-06 16:11:39 +0500

    1

--- mytest/tests/test_f_get_by_index.py
+++ mytest/tests/test_f_get_by_index.py
@@ -1,4 +1,4 @@
-from src.f_get_by_index import get_by_index
+# from src.f_get_by_index import get_by_index
 
-get_by_index(['a', 'b'], 1, 'c')
-get_by_index(['a', 'b'], 2, 'c')
+# get_by_index(['a', 'b'], 1, 'c')
+# get_by_index(['a', 'b'], 2, 'c')
