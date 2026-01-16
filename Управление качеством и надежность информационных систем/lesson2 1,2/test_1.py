f86702634235deb713af912a2dd4d97c2e268233
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-01-16 16:39:01 +0500

    unittest

---
+++ Управление качеством и надежность информационных систем/lesson2 1,2/test_1.py
@@ -0,0 +1,15 @@
+import unittest
+from math_utils import divide
+
+
+class TestDivide(unittest.TestCase):
+    def test_correct_division(self):
+        result = divide(10, 2)
+        self.assertEqual(result, 5)
+        
+    def test_division_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+            
+if __name__ == '__main__':
+    unittest.main()
