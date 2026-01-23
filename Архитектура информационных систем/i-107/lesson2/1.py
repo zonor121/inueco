e3dfc19cd16478fbdff272d6cce8a81c9cfd4243
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-01-23 10:49:09 +0500

    3 типа данных

--- Архитектура информационных систем/i-107/lesson2/1.py
+++ Архитектура информационных систем/i-107/lesson2/1.py
@@ -5,3 +5,30 @@ print(123)
 print(1231)
 print(12314)
 print(123441)
+
+# int - целые числа
+x = 10
+
+print(x)
+print(x + 5)
+print(x - 5)
+print(x * 5)
+print(x / 5)
+print(x // 5)
+print(x % 5)
+
+y = 15
+wpmlepmlewwpm = 1232132132132121
+
+print(y)
+print(wpmlepmlewwpm)
+print(x + y)
+
+# float - числа с плавающей точкой
+print(123.21312321)
+print(1.0)
+
+# str - строки
+print('Hello, World!')
+print("Hello, World!")
+print("!@#$%^&*()_+}{|?><M}")
