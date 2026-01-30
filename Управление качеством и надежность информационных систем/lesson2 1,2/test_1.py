4e92da60e416539b0658d619d14f25d18e6ad721
zonor121 <kopinator9000@gmail.com>
2026-01-30 17:49:28 +0500

    1

--- Управление качеством и надежность информационных систем/lesson2 1,2/test_1.py
+++ Управление качеством и надежность информационных систем/lesson2 1,2/test_1.py
@@ -11,18 +11,6 @@ class TestDivide(unittest.TestCase):
         with self.assertRaises(ValueError):
             divide(10, 0)
 
-    def test_correct_pow(self):
-        result = pow(5, 7)
-        self.assertEqual(result, 78125)
-
-    def test_pow_by_zero_b(self):
-        result = pow(10, 0)
-        self.assertEqual(result, 1)
-
-    def test_pow_by_less_then_zero_b(self):
-        result = pow(2, -1)
-        self.assertEqual(result, 0.5)
-
 #протестировать функцию pow
 
 
