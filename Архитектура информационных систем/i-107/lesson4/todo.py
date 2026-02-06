301d774a7e3008349f8b6b4cc733e8be897955f2
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-02-06 11:22:23 +0500

    todo

--- Архитектура информационных систем/i-107/lesson4/todo.py
+++ Архитектура информационных систем/i-107/lesson4/todo.py
@@ -2,10 +2,10 @@
 https://code-basics.com/ru
 до Экранированные последовательности. Не включая
 https://ru.pythontutor.ru/problem/old/1
-не выполняем
+до 4 не включая
 https://education.yandex.ru/handbook/python
-до 2. Базовые конструкции Python. Не включая
+до 2.3 Не включая
 https://stepik.org/catalog/131?free=true
 "Поколение Python": курс для начинающих 
-до. Не включая
+до 2.3 Не включая
 """
