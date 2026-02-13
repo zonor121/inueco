e493559655acb04f204905ee4ef67285306bc61d
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-02-13 10:39:47 +0500

    first

---
+++ Архитектура информационных систем/i-107/lesson5/add.py
@@ -0,0 +1,31 @@
+
+
+print('213', 'asas', 'asdasd', 2 + 6, True or False)
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, sep='-', end='')
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, sep='|', end='i14bf143bji3fb134f34f')
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, sep='~')
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, sep='*')
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, sep='.')
+
+a = input('Введите свой возраст: ')
+print('Вам', a, 'лет')
+a = int(a)
+print('через год', a + 1)
+
+print(type(a))
+
+nan = None
+a = 0
+b = ''
+c = 0.0
+
+
+int()
+float()
+str()
+bool()
+print(int('123'), type(int('123')))
+print(float('123.34'), type(float('123.34')))
+print(str(123), type(str(123)))
+print(bool("True"), type(bool("True")))
+
