1f1fa13fd6132fdb927c0243b9026a3e8657edb7
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-02-13 12:11:28 +0500

    строки

---
+++ Архитектура информационных систем/i-107/lesson5/strr.py
@@ -0,0 +1,20 @@
+print("1321322121dsfwewewewfw")
+print("""
+      123
+      21321
+      3213213
+      drfewdvfas
+      dvfaeveavfe""")
+a = """
+      123
+      21321
+      3213213"""
+print(a)
+print('123' + '21321')
+print('табуретка' + 'табуретка')
+print('табуретка' * 5)
+print("-" * 40)
+
+print("213 \n123")
+print("213 \\n 123")
+print("213 \t 123")
